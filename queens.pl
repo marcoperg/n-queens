f0ac662994f9e4393495e341557e1042771a6a3b
Marco <marcopg4@gmail.com>
2022-11-22 11:16:42 +0100

    Improve speed a lot by better using backtracking

--- queens.pl
+++ queens.pl
@@ -1,16 +1,15 @@
 :- module(queens, _, _).
 :- use_module(library(between)).
-:- use_module(library(write)).
-:- use_module(library(streams)).
 :- use_module(library(lists)).
 
-queens(N, L) :- integer(N), length(L, N), check_ints(N, L), check_queens(L, 0, L).
+queens(N, L) :- integer(N), length(L, N), check_queens(N, L, 0, L).
 
-check_queens(_, _, []).
-check_queens(L, QueenRow, [QueenCol | XS]) :- 
-                             Q1 is QueenRow+1,
+check_queens(_, _, _, []).
+check_queens(N, L, QueenRow, [QueenCol | XS]) :- 
+                             between(0, N-1, QueenCol),
                              check_is_attacked(QueenRow, QueenCol, 0, L),
-                             check_queens(L, Q1, XS).
+                             Q1 is QueenRow+1,
+                             check_queens(N, L, Q1, XS).
 
 check_is_attacked(_, _, _, []).
 
@@ -21,9 +20,4 @@ check_is_attacked(QueenRow, QueenCol, OtherQueenRow, [OtherQueenCol | XS]) :-
                                 Q1 is OtherQueenRow + 1,
                                 check_is_attacked(QueenRow, QueenCol, Q1, XS).
 
-check_is_attacked(QueenRow, QueenCol, QueenRow, [_ | XS]) :- 
-                                Q1 is QueenRow + 1,
-                                check_is_attacked(QueenRow, QueenCol, Q1, XS).
-
-check_ints(_, []).
-check_ints(N, [X | XS]) :- N1 is N-1, between(0, N1, X), check_ints(N, XS).
+check_is_attacked(QueenRow, _, QueenRow, _).
